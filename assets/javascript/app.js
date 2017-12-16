73c53dddc42829d100b32aed5edc9abf126c94ba
Ben French <ben.jminw@gmail.com>
2017-12-16 11:48:42 -0700

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,41 @@
-var config = {
-    apiKey: "AIzaSyB9GTc9gYfFFO0EuNHQJy9QAKzgYNMv_3I",
-    authDomain: "emptracker-a4659.firebaseapp.com",
-    databaseURL: "https://emptracker-a4659.firebaseio.com",
-    projectId: "emptracker-a4659",
-    storageBucket: "emptracker-a4659.appspot.com",
-    messagingSenderId: "837593961377"
-};
+$(document).ready(function () {
 
-firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyB9GTc9gYfFFO0EuNHQJy9QAKzgYNMv_3I",
+        authDomain: "emptracker-a4659.firebaseapp.com",
+        databaseURL: "https://emptracker-a4659.firebaseio.com",
+        projectId: "emptracker-a4659",
+        storageBucket: "emptracker-a4659.appspot.com",
+        messagingSenderId: "837593961377"
+    };
 
-const database = firebase.database();
-const ref = database.ref();
+    firebase.initializeApp(config);
 
-// Initial Values
-var EmpName = "";
-var EmpRole = "";
-var EmpRate = 0;
-var EmpDate = "";
+    const database = firebase.database();
+    const ref = database.ref();
+
+    // Initial Values
+    var EmpName = "";
+    var EmpRole = "";
+    var EmpRate = 0;
+    var EmpDate = "";
 
 
-ref.on("child_added", function (snapshot) {
-    // Log everything that's coming out of snapshot
-    console.log(snapshot.val());
-    // Change the HTML to reflect
-    let totalBilled = 0;
-    let startDate = 0;
-    let currentDate = 0;
-    let months = currentDate - startDate;
+    ref.on("child_added", function (snapshot) {
+        // Log everything that's coming out of snapshot
+        console.log(snapshot.val());
+        // Change the HTML to reflect
+        let totalBilled = 0;
+        let startDate = 0;
+        let currentDate = 0;
+        let months = currentDate - startDate;
 
-    EmpName = snapshot.val().EmpName;
-    EmpDate = snapshot.val().EmpDate;
-    EmpRole = snapshot.val().EmpRole;
-    EmpRate = snapshot.val().EmpRate;
-    
-    let print = `
+        EmpName = snapshot.val().EmpName;
+        EmpDate = snapshot.val().EmpDate;
+        EmpRole = snapshot.val().EmpRole;
+        EmpRate = snapshot.val().EmpRate;
+
+        let print = `
       <tr>
             <td scope="row">${EmpName}</td>
             <td>${EmpRole}</td>
@@ -42,24 +44,25 @@ ref.on("child_added", function (snapshot) {
             <td>${EmpRate}</td>
             <td>${totalBilled}</td>
         </tr>`;
-    $('#emp-table-body').append(print);
-    // Handle the errors
-}, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
-
-$('#add-employee-btn').on('click', function (event) {
-    event.preventDefault();
-
-    EmpName = $("#EmpName").val().trim();
-    EmpRole = $("#EmpRole").val().trim();
-    EmpDate = $("#EmpDate").val().trim();
-    EmpRate = $("#EmpRate").val().trim();
-
-    ref.push({
-        EmpName,
-        EmpRole,
-        EmpDate,
-        EmpRate
+        $('#emp-table-body').append(print);
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
-});
+
+    $('#add-employee-btn').on('click', function (event) {
+        event.preventDefault();
+
+        EmpName = $("#EmpName").val().trim();
+        EmpRole = $("#EmpRole").val().trim();
+        EmpDate = $("#EmpDate").val().trim();
+        EmpRate = $("#EmpRate").val().trim();
+
+        ref.push({
+            EmpName,
+            EmpRole,
+            EmpDate,
+            EmpRate
+        });
+    });
+})
